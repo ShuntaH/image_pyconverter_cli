80ff2d2a7a9691fafdf202a7b8eaa0543481f59e
ShuntaH <exec.apea@gmail.com>
2022-11-03 23:04:50 +0900

    FIX: arg_parser errors

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import argparse
 from enum import Enum
 
+from lib import rename
+
 
 def args_config():
     arg_parser = argparse.ArgumentParser()
+
+    # common
     arg_parser.add_argument('-dr', '--dryrun', action='store_true', default=True)
     arg_parser.add_argument(
         'task',
@@ -11,6 +15,10 @@ def args_config():
         type=str,
         choices=[task.value for task in Tasks]
     )
+
+    # rename module
+    arg_parser = rename.add_argument_to(arg_parser=arg_parser)
+
     args = arg_parser.parse_args()
     return args
 
@@ -22,14 +30,11 @@ class Tasks(Enum):
 def ic():
     args = args_config()
     task = args.task
-    tasks = [task.value for task in Tasks]
     dryrun = args.dryrun
     print(f'{task} task starts. [DRY-RUN: {dryrun}]')
 
     # taskに応じて呼び出すメソッドを変更する
     if task == Tasks.RENAME.value:
-        # rename task
-        from lib import rename
         rename.main(dryrun=dryrun)
 
     print(f'{task} task ends. [DRY-RUN: {dryrun}]')
