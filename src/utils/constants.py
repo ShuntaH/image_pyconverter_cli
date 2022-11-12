b651517c6b08e5220d2fd1e9b2b3d9728ffb5736
ShuntaH <exec.apea@gmail.com>
2022-11-13 00:42:36 +0900

    UPDATE: exclude src/utils from gitignore

---
+++ src/utils/constants.py
@@ -0,0 +1,17 @@
+VALID_EXTENSIONS = [
+    '.jpg',
+    '.jpeg',
+    '.JPG',
+    '.JPEG',
+    '.jpe',
+    '.jfif',
+    '.pjpeg',
+    '.pjp',
+    '.png',
+    '.gif',
+    '.tiff',
+    '.tif',
+    '.webp',
+    '.svg',
+    '.svgz'
+]
