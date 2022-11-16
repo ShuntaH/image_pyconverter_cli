86c14429a8957915809192b3f100681942ac0295
ShuntaH <exec.apea@gmail.com>
2022-11-16 14:46:06 +0900

    wip

--- src/tests/utils/test_with_statements.py
+++ src/tests/utils/test_with_statements.py
@@ -9,14 +9,16 @@ def test_add_extra_arguments_to():
     arg_parser = argparse.ArgumentParser()
 
     with add_extra_arguments_to(arg_parser) as arg_parser:
-        pass
-
-    ###############################################
-    # The following code should not be executed
-    # because an error will occur
-    # because the command is not actually entered.
-    # args = arg_parser.parse_args()
-    ###############################################
+        # todo 位置引数を指定しないとこのモジュールのパスがはいる
+        # todo デフォルトを指定すると位置引数が足りないエラーが起きる
+        arg_parser.add_argument(
+            'hoge',
+            default='hoge',
+            type=str,
+            help='e.g. /Users/macbook/images'
+        )
+        # hoge のdestに target actionの内容が勝手に入る
+        # テストではターミナルからの発火ではないのでダミーを用意しなくて都合が良いか
 
     actions = arg_parser._actions
 
