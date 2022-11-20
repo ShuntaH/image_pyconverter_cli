4f3f249d14502cecf2f8bf2bda9d608341034084
ShuntaH <exec.apea@gmail.com>
2022-11-20 23:14:47 +0900

    ADD: valid_image_paths_generator func

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,6 @@
 import pathlib
 import re
-from typing import Generator, Pattern
+from typing import Generator, Pattern, Union
 
 from src.utils.stdout import Stdout, Bcolors
 from utils.constants import VALID_EXTENSIONS
@@ -15,27 +15,39 @@ def get_image_paths_from_within_dir(
     ['/User/macbook/a.jpg', '/User/macbook/b.jpg', '/User/macbook/c.jpg']
     :return: list
     """
-    import os
-    os.path.ex
 
     dir_p: pathlib.PosixPath = pathlib.Path(dir_path)
-    dir_p_as_posix: str = dir_p.as_posix()
+    dir_p_string: str = dir_p.as_posix()
     if not dir_p.exists():
-        raise ValueError(f'"{dir_p_as_posix} does not exists."')
+        raise ValueError(f'"{dir_p_string} does not exists."')
 
     ext_pattern: Pattern = re.compile("/*(" + '|'.join(valid_extensions) + ")$")  # => /*(.jpg|.jpeg|.png)$
 
-    ps = []
+    g = valid_image_paths_generator(dir_path=dir_p, pattern=ext_pattern)
+
+    try:
+        g.__next__()
+    except StopIteration:
+        raise ValueError(f'No images within "{dir_p_string}".')
+
+    return g
+
+
+def valid_image_paths_generator(
+        dir_path: Union[str, pathlib.PosixPath],
+        pattern: Pattern
+) -> Generator:
+
+    dir_p = dir_path
+    if type(dir_path) is str:
+        dir_p = pathlib.Path(dir_p)
+
     for p in dir_p.glob('**/*'):
-        if not ext_pattern.search(dir_p_as_posix):
+        p_string = p.as_posix()
+        if not pattern.search(p_string):
             Stdout.styled_stdout(
                 Bcolors.WARNING.value,
-                f"'{dir_p_as_posix}' is invalid extension."
+                f"'{p_string}' is invalid extension."
             )
             continue
-        ps.append(p)
-
-    if not ps:
-        raise ValueError(f'No images within "{dir_p_as_posix}". {ps}')
-
-    return ps
+        yield p
