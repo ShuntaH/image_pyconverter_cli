17446a92e6c4b66d386c1eabf9dea77c05fb3722
ShuntaH <exec.apea@gmail.com>
2022-12-17 17:11:21 +0900

    UPDATE: apply get_styled_stdout_string to test_stdout_exception_message

--- src/tests/test_utils/test_stdout.py
+++ src/tests/test_utils/test_stdout.py
@@ -1,4 +1,5 @@
 from utils.stdout import Bcolors, stdout_exception_message
+from utils.tests import get_styled_stdout_string
 from utils.with_statements import stdout_to_text
 
 
@@ -10,4 +11,4 @@ def test_stdout_exception_message(temp_dir_path, temp_text_file):
     except ValueError as e:
         with stdout_to_text(text_file_path=_temp_text):
             stdout_exception_message(e)
-        assert _temp_text.read_text() == f"{Bcolors.FAIL.value}{exception_message}{Bcolors.ENDC.value}\n"
+        assert _temp_text.read_text() == get_styled_stdout_string(style=Bcolors.FAIL.value, sentence=exception_message)
--- src/utils/tests.py
+++ src/utils/tests.py
@@ -3,4 +3,4 @@ from utils import Bcolors
 
 def get_styled_stdout_string(style: Bcolors = Bcolors.NONE.value, sentence: str = "") -> str:  # type:ignore
     """Retrieve messages on standard output in a styled state."""
-    return f"{style}{sentence}{Bcolors.ENDC.value}"
+    return f"{style}{sentence}{Bcolors.ENDC.value}\n"
